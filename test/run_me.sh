1a560fbdf588e959e23de58dde1b95c3273ae25a
dstorey <dylan.storey@gmail.com>
2015-11-04 09:00:05 -0800

    changed chunking so that we don't create too many files for the file system , need to keep an eye on this (switch to a db for file storage later?)

--- test/run_me.sh
+++ test/run_me.sh
@@ -5,19 +5,14 @@
 ../Split_Manifests --files Genomes/*.fa --chunk_size 200
 
 #Run them from a single node 
-for i in *.man; do ../G2GCalc --threads 32 --manifest $i ; done 
+for i in *.man; do cd Chunk-$i; ../G2GCalc --threads 32 --manifest $i ;cd .. ; done 
 
 
 #SBATCH
 #cp ../Array_Submit.slrm . 
 #sbatch --array=0-2 Array_Submit.slrm
 
-
-#We should clean up a little once were done running
-mkdir co-ords
-mv *coords co-ords/
-mkdir deltas
-mv *delta deltas/
+cp Chunk*/*mtx . 
 
 #Combine and plot 
 #Get max elements 
